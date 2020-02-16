416339aba8f33bcbfd29763315e527fce174248f
Ben30000 <47725693+Ben30000@users.noreply.github.com>
2020-02-16 15:34:15 -0600

    Commit #1

--- app/src/main/java/com/example/lab3milestone2/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone2/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.lab3milestone2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button firstFragment, secondFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firstFragment = (Button) findViewById(R.id.firstFragment);
+        secondFragment = (Button) findViewById(R.id.secondFragment);
+
+        firstFragment.setOnClickListener(new View.OnClickListener() {
+        @Override
+            public void onClick(View v) {
+            loadFragment(new FirstFragment());
+        }
+        });
+
+        secondFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new SecondFragment());
+            }
+        });
+
+
     }
+
+
+        private void loadFragment(Fragment fragment) {
+        // create a FragmentManager
+            FragmentManager fm = getFragmentManager();
+            // create a FragmentTransaction to begin the transaction and replace the Fragment
+            FragmentTransaction fragmentTransaction = fm.beginTransaction();
+            // replace the FrameLayout with the new Fragment
+            fragmentTransaction.replace(R.id.frameLayout,fragment);
+            fragmentTransaction.commit();
+        }
 }
+
+
